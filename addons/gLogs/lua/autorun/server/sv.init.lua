b9d927050f82db2e586474e88194e9f62ec29b55
_FR_Starfox64 <louisdijon21@yahoo.fr>
2014-09-22 22:56:28 +0200

    Fixed hooks loading

--- addons/gLogs/lua/autorun/server/sv.init.lua
+++ addons/gLogs/lua/autorun/server/sv.init.lua
@@ -5,9 +5,9 @@ print("[gLogs] Initializing...")
 include("glogs/sv_config.lua")
 include("glogs/sv_glogs.lua")
 -- Load log hooks...
-local hooks = file.Find("gLogs/hooks/*.lua", "LUA")
+local hooks = file.Find("glogs/hooks/*.lua", "LUA")
 for k,v in pairs(hooks) do
-	include(v)
+	include("glogs/hooks/"..v)
 end
 -- Lets let them know that we are done.
 print("[gLogs] Initialized Version: "..gLogs.Version)
